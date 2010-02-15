2f8755888f595a7f8195f6b6c38c5f4313ad08cb
lado <herrlado@gmail.com>
2010-02-15 20:39:53 +0100

    refactor

--- jquery.geo.typing.js
+++ jquery.geo.typing.js
@@ -4,46 +4,48 @@
  * Thanks To Lado Kumsiashvili ( herrlado )
  * Version 0.1
  * Licensed under GPL:
- * http://www.gnu.org/licenses/gpl.html 
+ * http://www.gnu.org/licenses/gpl.html
  */
-(function($){  
-$.fn.geo = function(opt) {
-var def = {
-	turn: "on",
-};
-var opt = $.extend(def, opt);
-$(this).unbind();
-this.each(function() {
-	$(this).keypress(function(e) {
-		if(!e.ctrlKey && !e.altKey && !e.metaKey) { 
-			var code = (e.keyCode ? e.keyCode : e.which);
-			if(code > 64 && code <= 127) {
-				if(opt.turn == "off"){	return true; }else{
-					e.preventDefault();
-					var text = String.fromCharCode(ide(code));
-					if (document.selection) {
-						this.focus();
-						var sel = document.selection.createRange();
-						sel.select();
-						sel.text = text;
-						var lenght = sel.text.lenght;
-						sel.moveStart('character', text.length);
-						sel.moveEnd('character', text.length);
-					}else if(this.selectionStart || this.selectionStart == 0){
-						var start = this.selectionStart;
-						var end = this.selectionEnd;
-						var prefix = this.value.substring(0, start);
-						var suffix = this.value.substring(end,this.value.length);
-						var scrollTop = this.scrollTop;
-						this.value = prefix + text + suffix;
-						this.focus();
-						this.selectionStart = start + text.length;
-						this.selectionEnd = end + text.length;
-						this.scrollTop = scrollTop;
-					}else{
-						this.value+=text;
-					}
-}}}});});			
+(function($){
+    $.fn.geo = function(opt) {
+        var def = {
+            turn: "on",
+        };
+        var opt = $.extend(def, opt);
+        $(this).unbind();
+        this.each(function() {
+                $(this).keypress(function(e) {
+                        if(e.ctrlKey || e.altKey || e.metaKey) return;
+                        var code = (e.keyCode ? e.keyCode : e.which);
+                        if(code < 64 || code > 127) return;
+                        if(opt.turn == "off") return true;
+                        e.preventDefault();
+                        var text = String.fromCharCode(ide(code));
+                        if (document.selection) {
+                            this.focus();
+                            var sel = document.selection.createRange();
+                            sel.select();
+                            sel.text = text;
+                            var lenght = sel.text.lenght;
+                            sel.moveStart('character', text.length);
+                            sel.moveEnd('character', text.length);
+                        } else if(this.selectionStart || this.selectionStart == 0){
+                            var start = this.selectionStart;
+                            var end = this.selectionEnd;
+                            var prefix = this.value.substring(0, start);
+                            var suffix = this.value.substring(end,this.value.length);
+                            var scrollTop = this.scrollTop;
+                            this.value = prefix + text + suffix;
+                            this.focus();
+                            this.selectionStart = start + text.length;
+                            this.selectionEnd = end + text.length;
+                            this.scrollTop = scrollTop;
+                        } else {
+                            this.value+=text;
+                        }
+                    });
+            });
+
         // Mapping
         function ide(num){
             var value = lookUp[num];
@@ -59,5 +61,5 @@ this.each(function() {
             84:4311,  117:4323, 118:4309, 119:4332,
             87:4333,  120:4334, 121:4327, 122:4310, 90:4331
         };
-    }; 
+    };
 })(jQuery);
